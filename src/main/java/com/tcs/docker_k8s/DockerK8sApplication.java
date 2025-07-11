dab8ef4e31e3c5450509f2de8a9f784bdcc36198
DhanalakshmiThirumoorthi <dhanubtech95@gmail.com>
2025-07-11 15:37:16 +0530

    initial commit

---
+++ src/main/java/com/tcs/docker_k8s/DockerK8sApplication.java
@@ -0,0 +1,22 @@
+package com.tcs.docker_k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@SpringBootApplication
+public class DockerK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerK8sApplication.class, args);
+	}
+	
+	@GetMapping("/healthcheck")
+	public String health() {
+		return "<h1>Hi There!! Heath check is OK</h1>";
+	}
+
+}
---
+++ src/test/java/com/tcs/docker_k8s/DockerK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.docker_k8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerK8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
